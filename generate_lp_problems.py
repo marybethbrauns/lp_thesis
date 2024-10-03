8fa3bdc8349cc31923d03923a957b4f59abc52e0
Marybeth Brauns <marybethbrauns@gmail.com>
2024-10-03 13:17:36 -0400

    restructred the code, created comprehensive excel workbook, updated readMe.txt

--- generate_lp_problems.py
+++ generate_lp_problems.py
@@ -6,6 +6,10 @@ import pulp
 from deap import base, creator, tools, algorithms
 import random
 from sklearn.model_selection import train_test_split
+import time
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import Font, Alignment
 
 class IntegerLPProblem:
     def __init__(self, num_variables, num_constraints):
@@ -24,6 +28,7 @@ class IntegerLPProblem:
         self.bounds = [(0, 10) for _ in range(self.num_variables)]
 
     def solve(self):
+        start_time = time.time()
         prob = pulp.LpProblem("Integer LP Problem", pulp.LpMinimize)
         vars = [pulp.LpVariable(f'x{i}', lowBound=self.bounds[i][0], upBound=self.bounds[i][1], cat='Integer')
                 for i in range(self.num_variables)]
@@ -31,10 +36,12 @@ class IntegerLPProblem:
         for i in range(self.num_constraints):
             prob += pulp.lpSum(self.constraints[i, j] * vars[j] for j in range(self.num_variables)) <= self.constraints[i, -1]
         prob.solve()
+        end_time = time.time()
         self.solution = {
             'status': pulp.LpStatus[prob.status],
             'objective_value': pulp.value(prob.objective),
-            'variables': [var.varValue for var in prob.variables()]
+            'variables': [var.varValue for var in prob.variables()],
+            'solve_time': end_time - start_time
         }
 
     def to_dict(self):
@@ -68,141 +75,226 @@ class LPDatasetGenerator:
     def to_dataframe(self):
         return pd.DataFrame(self.problems)
 
-def evaluate(individual, problem):
-    objective = problem['objective']
-    constraints = problem['constraints']
+class GeneticAlgorithmSolver:
+    def __init__(self, pop_size=50, ngen=50, cxpb=0.7, mutpb=0.2):
+        self.pop_size = pop_size
+        self.ngen = ngen
+        self.cxpb = cxpb
+        self.mutpb = mutpb
 
-    obj_value = sum(x * c for x, c in zip(individual, objective))
+    @staticmethod
+    def evaluate(individual, problem):
+        objective = problem['objective']
+        constraints = problem['constraints']
 
-    penalty = 0
-    for constraint in constraints:
-        left_side = sum(x * c for x, c in zip(individual, constraint[:-1]))
-        if left_side > constraint[-1]:
-            penalty += (left_side - constraint[-1]) * 100
+        obj_value = sum(x * c for x, c in zip(individual, objective))
 
-    return obj_value + penalty,
+        penalty = 0
+        for constraint in constraints:
+            left_side = sum(x * c for x, c in zip(individual, constraint[:-1]))
+            if left_side > constraint[-1]:
+                penalty += (left_side - constraint[-1]) * 100
 
-def calculate_objective_value(solution, problem):
-    return sum(x * c for x, c in zip(solution, problem['objective']))
+        return obj_value + penalty,
 
-def create_toolbox(num_variables, bounds):
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-    creator.create("Individual", list, fitness=creator.FitnessMin)
+    @staticmethod
+    def calculate_objective_value(solution, problem):
+        return sum(x * c for x, c in zip(solution, problem['objective']))
 
-    toolbox = base.Toolbox()
-    toolbox.register("attr_int", random.randint, bounds[0], bounds[1])
-    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, n=num_variables)
-    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    def create_toolbox(self, num_variables, bounds):
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+        creator.create("Individual", list, fitness=creator.FitnessMin)
 
-    toolbox.register("evaluate", evaluate)
-    toolbox.register("mate", tools.cxTwoPoint)
-    toolbox.register("mutate", tools.mutUniformInt, low=bounds[0], up=bounds[1], indpb=0.1)
-    toolbox.register("select", tools.selTournament, tournsize=3)
+        toolbox = base.Toolbox()
+        toolbox.register("attr_int", random.randint, bounds[0], bounds[1])
+        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, n=num_variables)
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-    return toolbox
+        toolbox.register("evaluate", self.evaluate)
+        toolbox.register("mate", tools.cxTwoPoint)
+        toolbox.register("mutate", tools.mutUniformInt, low=bounds[0], up=bounds[1], indpb=0.1)
+        toolbox.register("select", tools.selTournament, tournsize=3)
 
-def solve_with_ga(problem, pop_size=50, ngen=50, cxpb=0.7, mutpb=0.2):
-    num_variables = problem['num_variables']
-    bounds = problem['bounds'][0]  # Assuming all variables have the same bounds
+        return toolbox
 
-    toolbox = create_toolbox(num_variables, bounds)
-    toolbox.register("evaluate", evaluate, problem=problem)
+    def solve(self, problem):
+        num_variables = problem['num_variables']
+        bounds = problem['bounds'][0]  # Assuming all variables have the same bounds
 
-    pop = toolbox.population(n=pop_size)
-    hof = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", np.mean)
-    stats.register("min", np.min)
-    stats.register("max", np.max)
+        toolbox = self.create_toolbox(num_variables, bounds)
+        toolbox.register("evaluate", self.evaluate, problem=problem)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=cxpb, mutpb=mutpb, ngen=ngen,
-                                   stats=stats, halloffame=hof, verbose=False)
+        pop = toolbox.population(n=self.pop_size)
+        hof = tools.HallOfFame(1)
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", np.mean)
+        stats.register("min", np.min)
+        stats.register("max", np.max)
 
-    return {
-        'best_solution': hof[0],
-        'best_fitness': hof[0].fitness.values[0],
-        'predicted_objective': calculate_objective_value(hof[0], problem)
-    }
+        start_time = time.time()
+        pop, log = algorithms.eaSimple(pop, toolbox, cxpb=self.cxpb, mutpb=self.mutpb,
+                                       ngen=self.ngen, stats=stats, halloffame=hof, verbose=False)
+        end_time = time.time()
 
-def process_dataset(df, is_training=True):
-    results = []
-    for _, problem in tqdm(df.iterrows(), total=len(df), desc="Processing"):
-        ga_result = solve_with_ga(problem)
-        results.append({
-            'problem_id': _,
-            'best_solution': ga_result['best_solution'],
-            'best_fitness': ga_result['best_fitness'],
-            'predicted_objective': ga_result['predicted_objective'],
-            'optimal_solution': problem['solution']['variables'],
-            'optimal_objective': problem['solution']['objective_value']
+        return {
+            'best_solution': hof[0],
+            'best_fitness': hof[0].fitness.values[0],
+            'predicted_objective': self.calculate_objective_value(hof[0], problem),
+            'solve_time': end_time - start_time
+        }
+
+class Experiment:
+    def __init__(self, num_problems, min_variables, max_variables, min_constraints, max_constraints):
+        self.dataset_generator = LPDatasetGenerator(num_problems, min_variables, max_variables,
+                                                    min_constraints, max_constraints)
+        self.ga_solver = GeneticAlgorithmSolver()
+        self.problems_df = None
+        self.train_df = None
+        self.test_df = None
+        self.train_results = None
+        self.test_results = None
+        self.results_combined = None
+
+    def generate_dataset(self):
+        self.dataset_generator.generate_dataset()
+        self.problems_df = self.dataset_generator.to_dataframe()
+
+    def split_dataset(self, test_size=0.2, random_state=42):
+        self.train_df, self.test_df = train_test_split(self.problems_df, test_size=test_size, random_state=random_state)
+
+    def process_dataset(self, df, is_training=True):
+        results = []
+        for _, problem in tqdm(df.iterrows(), total=len(df), desc="Processing"):
+            ga_result = self.ga_solver.solve(problem)
+            results.append({
+                'problem_id': _,
+                'best_solution': ga_result['best_solution'],
+                'best_fitness': ga_result['best_fitness'],
+                'predicted_objective': ga_result['predicted_objective'],
+                'ga_solve_time': ga_result['solve_time'],
+                'optimal_solution': problem['solution']['variables'],
+                'optimal_objective': problem['solution']['objective_value'],
+                'pulp_solve_time': problem['solution']['solve_time']
+            })
+        return pd.DataFrame(results)
+
+    def run_experiment(self):
+        self.generate_dataset()
+        self.split_dataset()
+        print(f"Training set size: {len(self.train_df)}")
+        print(f"Testing set size: {len(self.test_df)}")
+        self.train_results = self.process_dataset(self.train_df, is_training=True)
+        self.test_results = self.process_dataset(self.test_df, is_training=False)
+
+    def calculate_metrics(self):
+        train_mae = np.mean(np.abs(self.train_results['predicted_objective'] - self.train_results['optimal_objective']))
+        test_mae = np.mean(np.abs(self.test_results['predicted_objective'] - self.test_results['optimal_objective']))
+        print(f"\nTraining Mean Absolute Error: {train_mae}")
+        print(f"Testing Mean Absolute Error: {test_mae}")
+
+        self.results_combined = pd.DataFrame({
+            'problem_id': self.test_results['problem_id'],
+            'predicted_objective_value': self.test_results['predicted_objective'],
+            'actual_objective_value': self.test_results['optimal_objective'],
+            'solution_fitness': self.test_results['best_fitness'],
+            'ga_solve_time': self.test_results['ga_solve_time'],
+            'pulp_solve_time': self.test_results['pulp_solve_time']
         })
-    return pd.DataFrame(results)
+
+        self.results_combined['time_difference'] = self.results_combined['pulp_solve_time'] - self.results_combined['ga_solve_time']
+        self.results_combined['ga_faster'] = self.results_combined['time_difference'] > 0
+        self.results_combined['speedup_factor'] = self.results_combined['pulp_solve_time'] / self.results_combined['ga_solve_time']
+
+        mismatches = self.results_combined[self.results_combined['predicted_objective_value'] != self.results_combined['actual_objective_value']]
+        total_problems = len(self.results_combined)
+        mismatch_percentage = (len(mismatches) / total_problems) * 100
+        overall_avg_difference = np.mean(np.abs(self.results_combined['predicted_objective_value'] - self.results_combined['actual_objective_value']))
+        ga_faster_count = self.results_combined['ga_faster'].sum()
+        avg_speedup = self.results_combined['speedup_factor'].mean()
+        median_speedup = self.results_combined['speedup_factor'].median()
+
+        return {
+            'train_mae': train_mae,
+            'test_mae': test_mae,
+            'mismatch_percentage': mismatch_percentage,
+            'overall_avg_difference': overall_avg_difference,
+            'ga_faster_count': ga_faster_count,
+            'ga_faster_percentage': (ga_faster_count / total_problems) * 100,
+            'avg_speedup': avg_speedup,
+            'median_speedup': median_speedup,
+            'mismatches': mismatches,
+            'total_problems': total_problems
+        }
+
+    def generate_excel_report(self, metrics):
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Overall Metrics"
+
+        metrics_data = [
+            ("Training Mean Absolute Error", metrics['train_mae']),
+            ("Testing Mean Absolute Error", metrics['test_mae']),
+            ("Percentage of problems where GA didn't find the optimal solution", metrics['mismatch_percentage']),
+            ("Overall average absolute difference", metrics['overall_avg_difference']),
+            ("Number of problems where GA was faster", metrics['ga_faster_count']),
+            ("Percentage of problems where GA was faster", metrics['ga_faster_percentage']),
+            ("Average speedup factor (PuLP time / GA time)", metrics['avg_speedup']),
+            ("Median speedup factor", metrics['median_speedup'])
+        ]
+
+        for row, (metric, value) in enumerate(metrics_data, start=1):
+            ws.cell(row=row, column=1, value=metric)
+            ws.cell(row=row, column=2, value=value)
+            ws.cell(row=row, column=2).number_format = '0.00'
+
+        ws_combined = wb.create_sheet("Combined Results")
+        for r in dataframe_to_rows(self.results_combined, index=False, header=True):
+            ws_combined.append(r)
+
+        ws_top_speedup = wb.create_sheet("Top 5 Speedup")
+        top_speedup = self.results_combined.nlargest(5, 'speedup_factor')[['problem_id', 'speedup_factor', 'ga_solve_time', 'pulp_solve_time']]
+        for r in dataframe_to_rows(top_speedup, index=False, header=True):
+            ws_top_speedup.append(r)
+
+        ws_bottom_speedup = wb.create_sheet("Bottom 5 Speedup")
+        bottom_speedup = self.results_combined.nsmallest(5, 'speedup_factor')[['problem_id', 'speedup_factor', 'ga_solve_time', 'pulp_solve_time']]
+        for r in dataframe_to_rows(bottom_speedup, index=False, header=True):
+            ws_bottom_speedup.append(r)
+
+        ws_mismatches = wb.create_sheet("Mismatches")
+        if not metrics['mismatches'].empty:
+            for r in dataframe_to_rows(metrics['mismatches'], index=False, header=True):
+                ws_mismatches.append(r)
+        else:
+            ws_mismatches['A1'] = "No mismatches found. The genetic algorithm found the optimal solution for all test problems."
+
+        for sheet in wb:
+            for cell in sheet[1]:
+                cell.font = Font(bold=True)
+            for column in sheet.columns:
+                max_length = 0
+                column_letter = column[0].column_letter
+                for cell in column:
+                    try:
+                        if len(str(cell.value)) > max_length:
+                            max_length = len(cell.value)
+                    except:
+                        pass
+                adjusted_width = (max_length + 2)
+                sheet.column_dimensions[column_letter].width = adjusted_width
+
+        excel_filename = '/Users/marybeth/Desktop/p_solver_results.xlsx'
+        wb.save(excel_filename)
+        print(f"\nKey metrics and results have been saved to {excel_filename}")
 
 if __name__ == "__main__":
     np.random.seed(42)
     random.seed(42)
 
-    # Generate a dataset of problems
-    generator = LPDatasetGenerator(num_problems=1000, min_variables=2, max_variables=5,
-                                   min_constraints=2, max_constraints=5)
-    generator.generate_dataset()
+    experiment = Experiment(num_problems=1000, min_variables=2, max_variables=5,
+                            min_constraints=2, max_constraints=5)
+    experiment.run_experiment()
+    metrics = experiment.calculate_metrics()
+    experiment.generate_excel_report(metrics)
 
-    # Create a pandas DataFrame from the generated problems
-    problems_df = generator.to_dataframe()
-
-    # Split the data into training and testing sets
-    train_df, test_df = train_test_split(problems_df, test_size=0.2, random_state=42)
-
-    print(f"Training set size: {len(train_df)}")
-    print(f"Testing set size: {len(test_df)}")
-
-    # Process training and testing datasets
-    train_results = process_dataset(train_df, is_training=True)
-    test_results = process_dataset(test_df, is_training=False)
-
-    # Calculate and print the mean absolute error for training and testing
-    train_mae = np.mean(np.abs(train_results['predicted_objective'] - train_results['optimal_objective']))
-    test_mae = np.mean(np.abs(test_results['predicted_objective'] - test_results['optimal_objective']))
-
-    print(f"\nTraining Mean Absolute Error: {train_mae}")
-    print(f"Testing Mean Absolute Error: {test_mae}")
-
-    # Combine results
-    results_combined = pd.DataFrame({
-        'problem_id': test_results['problem_id'],
-        'predicted_objective_value': test_results['predicted_objective'],
-        'actual_objective_value': test_results['optimal_objective'],
-        'solution_fitness': test_results['best_fitness']
-    })
-
-    # Print combined results
-    print("\nCombined results of predicted and actual optimal solutions:")
-    print(results_combined)
-
-
-    # Optionally, save the results to a CSV file
-    results_combined.to_csv('/Users/marybeth/Desktop/results.combined.csv', index=False)
-
-    # Print rows where predicted != actual
-    mismatches = results_combined[
-        results_combined['predicted_objective_value'] != results_combined['actual_objective_value']]
-
-    print("\nRows where predicted objective value doesn't match actual objective value:")
-    if mismatches.empty:
-        print("No mismatches found. The genetic algorithm found the optimal solution for all test problems.")
-    else:
-        print(mismatches)
-        print(f"\nNumber of mismatches: {len(mismatches)} out of {len(results_combined)} test problems")
-
-        # Calculate and print the average difference for mismatches
-        avg_difference = np.mean(np.abs(mismatches['predicted_objective_value'] - mismatches['actual_objective_value']))
-        print(f"Average absolute difference in mismatches: {avg_difference:.4f}")
-
-    # Calculate and print overall statistics
-    total_problems = len(results_combined)
-    mismatch_percentage = (len(mismatches) / total_problems) * 100
-    print(f"\nPercentage of problems where GA didn't find the optimal solution: {mismatch_percentage:.2f}%")
-
-    overall_avg_difference = np.mean(
-        np.abs(results_combined['predicted_objective_value'] - results_combined['actual_objective_value']))
-    print(f"Overall average absolute difference: {overall_avg_difference:.4f}")
+    print("\nExperiment completed. Check the Excel file for detailed results.")
--- readMe.txt
+++ readMe.txt
@@ -1,4 +1,108 @@
 Mary Elizabeth Brauns Senior Thesis with Dr. David Murphy
 Hillsdale College Department of Mathematics
 
-generate_lp_problems.py creates and stores integer lp programming problems to be used to train the model
+# Integer Linear Programming Solver using Genetic Algorithm
+
+## Overview
+
+This project implements an Integer Linear Programming (ILP) solver using a Genetic Algorithm (GA) and compares its performance with a traditional exact solver (PuLP). The main goal is to evaluate the efficiency and accuracy of the GA approach in solving ILP problems.
+
+## Background
+
+### Integer Linear Programming (ILP)
+
+Integer Linear Programming is a mathematical optimization technique used to find the best solution to a problem with the following characteristics:
+
+1. The objective function is linear (e.g., maximize profit or minimize cost).
+2. The constraints are linear inequalities or equalities.
+3. Some or all of the variables are restricted to integer values.
+
+ILP is widely used in various fields, including operations research, economics, and computer science, to solve complex optimization problems such as resource allocation, scheduling, and network flow problems.
+
+### PuLP
+
+PuLP is an open-source linear programming library for Python. It provides a user-friendly interface to define and solve linear programming and integer programming problems. Key features of PuLP include:
+
+1. Support for both continuous and integer variables.
+2. Ability to interface with various solvers (e.g., GLPK, CPLEX, Gurobi).
+3. Simple and intuitive syntax for defining objective functions and constraints.
+4. Capability to handle large-scale optimization problems.
+
+In this project, we use PuLP as the exact solver to find optimal solutions for our ILP problems, which we then compare with the solutions found by our genetic algorithm approach.
+
+## Key Components
+
+1. **IntegerLPProblem**: A class that represents an individual ILP problem. It can generate random problems and solve them using PuLP.
+
+2. **LPDatasetGenerator**: A class that generates a dataset of ILP problems.
+
+3. **Genetic Algorithm Solver**: Implemented using the DEAP library, this solver attempts to find optimal or near-optimal solutions to ILP problems.
+
+4. **Performance Comparison**: The script compares the GA solutions with exact solutions from PuLP in terms of both accuracy and solve time.
+
+## Main Functions
+
+- `evaluate()`: Fitness function for the GA.
+- `calculate_objective_value()`: Calculates the objective value of a solution.
+- `create_toolbox()`: Sets up the DEAP toolbox for the GA.
+- `solve_with_ga()`: Solves a single problem using the GA.
+- `process_dataset()`: Processes a set of problems using the GA.
+
+## Workflow
+
+1. Generate a dataset of ILP problems.
+2. Split the dataset into training and testing sets.
+3. Solve problems using both GA and PuLP.
+4. Compare results in terms of solution quality and solve time.
+5. Output detailed statistics and analysis.
+
+## How to Use
+
+1. Ensure all required libraries are installed:
+   ```
+   pip install numpy pandas tqdm pulp deap scikit-learn
+   ```
+
+2. Run the script:
+   ```
+   python <script_name>.py
+   ```
+
+3. The script will generate problems, solve them, and output results to the console. It will also save a CSV file with detailed results.
+
+## Output
+
+The script provides comprehensive output, including:
+
+- Mean Absolute Error for training and testing sets
+- Detailed results for each problem
+- Analysis of mismatches between GA and optimal solutions
+- Efficiency comparison between GA and PuLP
+- Statistics on solve times and speedup factors
+
+## Customization
+
+You can customize the following parameters in the `__main__` section:
+
+- Number of problems to generate
+- Range of variables and constraints
+- GA parameters (population size, number of generations, etc.)
+
+## Notes
+
+- The GA may not always find the optimal solution but can be faster than exact methods for some problems.
+- The efficiency of the GA vs. PuLP can vary depending on the problem characteristics.
+- This implementation is for educational and comparative purposes and may not be optimized for large-scale industrial use.
+- While PuLP guarantees finding the optimal solution (if one exists), it may become computationally expensive for large or complex problems. The GA approach trades off some accuracy for potentially faster solve times, especially on larger problems.
+
+## Future Improvements
+
+- Implement more sophisticated GA operators
+- Add support for different types of ILP problems (e.g., mixed-integer programming)
+- Optimize GA parameters automatically based on problem characteristics
+- Parallelize GA operations for improved performance
+- Incorporate other metaheuristic algorithms for comparison (e.g., Simulated Annealing, Particle Swarm Optimization)
+
+## Contributing
+
+Contributions to improve the code or extend its functionality are welcome. Please submit pull requests or open issues for any bugs or feature requests.
